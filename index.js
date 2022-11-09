644756d34bf5ef1cb739d7bb0bf249589b5756e0
راكينول إسلام <you@example.com>
2022-11-09 12:24:34 +0600

    mongo setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config();
 const app = express();
 
@@ -8,6 +9,16 @@ const port = process.env.PORT || 5000;
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.avq6v53.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+ 
 
 
 
